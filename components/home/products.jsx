b4f47a5d3acd1a2d78180180f308d0b99e855880
David's Devel <djgm1206@gmail.com>
2023-02-22 15:50:54 -0400

    Linted code

--- components/home/products.jsx
+++ components/home/products.jsx
@@ -4,8 +4,8 @@ export default function Products({data}) {
   console.log(data[0])
   return <div>
     <h3 className='my-4 font-bold text-2xl text-center'>Ultimos productos</h3>
-    <ul className='flex flex-wrap w-full justify-around'>
-      {data .map(e => <li key={e.title + e.id} className='w-1/2 my-4'>
+    <ul className='flex flex-wrap w-full justify-around max-w-7xl m-auto'>
+      {data .map(e => <li key={e.title + e.id} className='w-1/2 my-4 sm:w-1/3  md:w-1/4'>
         <div className='bg-slate-100 w-11/12 m-auto px-3 py-6 flex flex-col items-center'>
           <img src={e.image}/>
           <span className='text-center my-4'>{e.title}</span>
