5e38b88a487fb236f241cf0ff0e58c50d43e273f
David's Devel <djgm1206@gmail.com>
2023-03-08 13:35:53 -0400

    refactor: Updated landing UI

--- components/home/header.jsx
+++ components/home/header.jsx
@@ -1,29 +1,16 @@
-import {useState, useEffect, useRef} from 'react';
-
-const images = [
-  {
-    text: '',
-    image: '/images/ferreteria-cabecera-1.jpg'
-  },
-  {
-    text: '',
-    image: '/images/ferreteria-cabecera-2.jpg'
-  }
-];
+import Router from 'next/router';
+import Button from '@/components/button';
 
 export default function Header() {
-  const [active, setActive] = useState(0);
-  const intervalRef = useRef(null);
-
-  useEffect(() => {
-    setTimeout(() => setActive(active === 1 ? 0 : active + 1), 5000);
-  }, [active]);
-
-  return <header className='w-full flex h-80 relative'>
-    {
-      images.map((e, i) => <div key={e.image} className={` top-0 transition-all duration-300 ease w-full h-80 absolute bg-center bg-cover top-0 left-0 ${active === i ? 'opacity-100' : 'opacity-0'}`} style={{backgroundImage:  `url(${e.image})`}}>
-        
-      </div>)
-    }
+  return <header className='w-full flex h-[35rem] relative'>
+    <div className='top-0 transition-all duration-300 ease w-full h-full absolute bg-center bg-cover top-0 left-0' style={{backgroundImage: 'url(https://images.unsplash.com/photo-1602052793312-b99c2a9ee797?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&w=870&q=80)'}}>
+      <div className='bg-[#0007] absolute w-full h-full flex items-center px-4 md:px-12 lg:px-24'>
+        <div className='text-white w-full max-w-lg flex flex-col'>
+          <span className='text-xl'>En articulos de ferreteria, accesorios para el hogar y artículos para tu bicicleta. simplemente </span>
+          <span className='font-bold my-4 text-6xl'>Yo Soy</span>
+          <Button className='bg-red-500 w-48 mt-12' onClick={() => Router.push('/productos')}>Ver productos</Button>
+        </div>
+      </div>
+    </div>
   </header>;
 }
--- components/home/products.jsx
+++ components/home/products.jsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 import Card from './productCard';
 
 export default function Products({data}) {
-  return <div>
+  return <div className='py-12'>
     <h3 className='my-4 font-bold text-2xl text-center'>Ultimos productos</h3>
     <ul className='flex flex-wrap w-full justify-around max-w-7xl m-auto'>
       {
-        data.map((e, i) => <Card key={e.name + i} id={e.key} {...e}/>)
+        data.concat(data).map((e, i) => <Card key={e.name + i} id={e.key} {...e}/>)
       }
     </ul>
   </div>;
