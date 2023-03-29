029f20a5b29ba88bffbb3f74fb165740265531fe
David's Devel <djgm1206@gmail.com>
2023-03-29 14:21:48 -0400

    chore

--- pages/api/unsplash.js
+++ pages/api/unsplash.js
@@ -7,28 +7,37 @@ export default async function UnsplashAPI(req, res) {
     limit
   } = req.query;
 
-  const serverApi = createApi({
-    accessKey: process.env.UNSPLASH_KEY
-  });
+  try {
+    const serverApi = createApi({
+      accessKey: process.env.UNSPLASH_KEY
+    });
 
-  const photos = await serverApi.search.getPhotos({
-    query: q,
-    page: page ? +page : 1,
-    perPage: limit ? +limit : 10,
-    orientation: 'landscape',
-    lang: 'es'
-  });
+    const photos = await serverApi.search.getPhotos({
+      query: q,
+      page: page ? +page : 1,
+      perPage: limit ? +limit : 10,
+      orientation: 'landscape',
+      lang: 'es'
+    });
 
-  const {response} = photos;
+    const {response} = photos;
 
-  response.results = response.results.map(e => {
-    return {
-      url: e.urls.regular,
-      thumbnail: e.urls.thumb,
-      raw: e.urls.raw,
-      download: e.links.download_location
-    };
-  });
+    response.results = response.results.map(e => {
+      return {
+        url: e.urls.regular,
+        thumbnail: e.urls.thumb,
+        raw: e.urls.raw,
+        download: e.links.download_location
+      };
+    });
 
-  res.json(response);
+    res.json(response);
+  } catch(err){
+    if (err.message === 'expected JSON response from server.')
+      return res.json({
+        results: []
+      });
+
+    throw err;
+  }
 }
